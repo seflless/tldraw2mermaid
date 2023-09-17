8883d6f6dba015386bc9f48b6af7829133bf4b2a
Francois <francoislaberge@gmail.com>
2023-09-17 17:43:34 -0400

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   build: {
     rollupOptions: {
       plugins: [inject({ Buffer: ["Buffer", "Buffer"] })],
