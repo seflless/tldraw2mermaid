2bff852c689c8dec22ca7f171fb9c33f7487ff9b
Francois <francoislaberge@gmail.com>
2023-09-17 17:41:47 -0400

    Adds instructions and builds latest demo

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Draw Mermaid Diagrams using Tldraw
+
+# Contributing
+
+```
+git clone git@github.com:seflless/tldraw2mermaid.git
+cd tldraw2mermaid
+pnpm install
+```
+
+## Dev Mode
+
+```
+pnpm dev
+# open http://localhost:3000/
+```
+
+## Updating Github Pages
+
+Generates the static website and puts it into the demo folder.
+
+```
+pnpm demo
+```
--- src/mermaidIt.js
+++ src/mermaidIt.js
@@ -39,13 +39,26 @@ export function mermaidIt(records) {
         id: record.id,
       };
       nodes.push(node);
+    } else if (record.props?.geo === "diamond") {
+      const node = {
+        type: "diamond",
+        text: record.props?.text || " ",
+        id: record.id,
+      };
+      nodes.push(node);
     }
   }
 
   // Using this mermaid flowchart as a reference: https://mermaid.live/edit#pako:eNpdkNFLwzAQxv-VkCeFbXWtdlsRwXVzLwMFfWv3cGuSJtgmIb06Rtv_3dTJQPN0-X7f3XFfRwvDOE2oqMypkOCQfGxyTfx7zlLpVIM1NIdfhUynT_2OI6mN5ueerG92hjTSWKt0eXsxrcmjd5G0248-TlAq_TlcWPrD-lfNe7LJ9mDR2MMf9HEyPdlm6k36Df-QdNz3vWQCEgHTAhxJwR3ohNbc1aCYv6IbO3KKktc8p4kvGRfQVpjTXA_eCi2a97MuaIKu5RPaWgbINwpKBzX1c6vmqm6ZQuOuYmWAcf_tKJ7tGFnpw_EjC6OFKke9dZWXJaJtkiAY8axUKNvjrDB10Cg25iu_VnEQh_ESwojHiwgeoogVx_lqKcL7uWCLu3kIdBiGbySmhBs
   let nodesContent = "";
   nodes.forEach((node) => {
-    nodesContent += `\n   ${node.id}["${node.text}"]`;
+    if (node.type === "ellipse") {
+      nodesContent += `\n   ${node.id}(("${node.text}"))`;
+    } else if (node.type === "rectangle") {
+      nodesContent += `\n   ${node.id}("${node.text}")`;
+    } else if (node.type === "diamond") {
+      nodesContent += `\n   ${node.id}{"${node.text}"}`;
+    }
   });
   if (nodes.length && arrows.length) {
     nodesContent += "\n";
